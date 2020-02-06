abbbb58ee5c5dfc21b72a32eb75ff5e8f2dd8a26
vitaminwater <stantin04@gmail.com>
2020-02-06 19:20:15 +0100

    WIP - better add box process

--- lib/pages/add_box/select_device_box/select_device_box_bloc.dart
+++ lib/pages/add_box/select_device_box/select_device_box_bloc.dart
@@ -10,28 +10,26 @@ class SelectDeviceBoxBlocEventInitialize extends SelectDeviceBoxBlocEvent {
   List<Object> get props => [];
 }
 
-class SelectDeviceBoxBlocEventSetDeviceBox extends SelectDeviceBoxBlocEvent {
-  final int deviceBox;
-  SelectDeviceBoxBlocEventSetDeviceBox(this.deviceBox);
-
-  @override
-  List<Object> get props => [deviceBox];
-}
-
 class SelectDeviceBoxBlocState extends Equatable {
+  final List<int> leds;
+
+  SelectDeviceBoxBlocState(this.leds);
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [leds];
 }
 
 class SelectDeviceBoxBloc
     extends Bloc<SelectDeviceBoxBlocEvent, SelectDeviceBoxBlocState> {
+  List<int> _leds = [];
   final MainNavigateToSelectBoxDeviceBoxEvent _args;
 
-  SelectDeviceBoxBloc(this._args);
+  SelectDeviceBoxBloc(this._args) {
+    add(SelectDeviceBoxBlocEventInitialize());
+  }
 
   @override
-  SelectDeviceBoxBlocState get initialState =>
-      SelectDeviceBoxBlocState();
+  SelectDeviceBoxBlocState get initialState => SelectDeviceBoxBlocState(_leds);
 
   @override
   Stream<SelectDeviceBoxBlocState> mapEventToState(
@@ -39,21 +37,23 @@ class SelectDeviceBoxBloc
     if (event is SelectDeviceBoxBlocEventInitialize) {
       final ddb = RelDB.get().devicesDAO;
       final boxModule = await ddb.getModule(_args.device.id, 'box');
-      final availableBoxes = List<int>();
+      final boxes = List<int>();
       for (int i = 0; i < boxModule.arrayLen; ++i) {
-        final boxEnabled = await ddb.getParam(_args.device.id, 'BOX_${i}_ENABLED');
-        if (boxEnabled.ivalue != 0) {
-          availableBoxes.add(i);
+        final boxEnabled =
+            await ddb.getParam(_args.device.id, 'BOX_${i}_ENABLED');
+        if (boxEnabled.ivalue == 0) {
+          boxes.add(i);
         }
       }
       final ledModule = await ddb.getModule(_args.device.id, 'led');
-      final availableLeds = List<int>();
+      final leds = List<int>();
       for (int i = 0; i < ledModule.arrayLen; ++i) {
         final ledBox = await ddb.getParam(_args.device.id, 'LED_${i}_BOX');
-        if (availableBoxes.contains(ledBox.ivalue)) {
-          availableLeds.add(i);
+        if (boxes.contains(ledBox.ivalue)) {
+          leds.add(i);
         }
       }
+      yield SelectDeviceBoxBlocState(leds);
     }
   }
 }
--- lib/pages/add_box/select_device_box/select_device_box_page.dart
+++ lib/pages/add_box/select_device_box/select_device_box_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:super_green_app/main/main_navigator_bloc.dart';
 import 'package:super_green_app/pages/add_box/select_device_box/select_device_box_bloc.dart';
 import 'package:super_green_app/widgets/appbar.dart';
+import 'package:super_green_app/widgets/green_button.dart';
+import 'package:super_green_app/widgets/section_title.dart';
 
 class SelectDeviceBoxPage extends StatefulWidget {
   @override
@@ -17,17 +20,69 @@ class SelectDeviceBoxPageState extends State<SelectDeviceBoxPage> {
         bloc: Provider.of<SelectDeviceBoxBloc>(context),
         builder: (context, state) => Scaffold(
             appBar: SGLAppBar('Device configuration'),
-            body: Row(
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () => _handleInput(context),
-                  child: Text('CREATE BOX'),
-                ),
-              ],
-            )));
+            body: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: SectionTitle(
+                        title: 'Available LED channels',
+                        icon: 'assets/box_setup/icon_controller.svg',
+                      ),
+                    ),
+                    _renderBoxes(state),
+                    Expanded(
+                      child: Container(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: GreenButton(
+                          title: 'CREATE BOX',
+                          onPressed: () => _handleInput(context),
+                        ),
+                      ),
+                    ),
+                  ],
+                ))));
+  }
+
+  Widget _renderBoxes(state) {
+    return Row(
+      children: state.leds
+          .map<Widget>((led) => _renderBox(
+              context,
+              () {},
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+                child: SvgPicture.asset('assets/feed_form/icon_add.svg'),
+              )))
+          .toList(),
+    );
   }
 
   void _handleInput(BuildContext context) {
-    BlocProvider.of<MainNavigatorBloc>(context).add(MainNavigatorActionPop(param: 0));
+    BlocProvider.of<MainNavigatorBloc>(context)
+        .add(MainNavigatorActionPop(param: 0));
+  }
+
+  Widget _renderBox(BuildContext context, Function onPressed, Widget content) {
+    return SizedBox(
+        width: 70,
+        height: 80,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0, left: 8.0),
+          child: RawMaterialButton(
+            onPressed: onPressed,
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(10)),
+                child: content),
+          ),
+        ));
   }
 }
