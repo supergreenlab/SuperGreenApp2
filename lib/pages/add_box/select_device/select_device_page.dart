dc4c277a1f1155e19cf2487151606385c0780906
vitaminwater <stantin04@gmail.com>
2020-02-26 15:39:14 +0100

    WIP - box setup creation

--- lib/pages/add_box/select_device/select_device_page.dart
+++ lib/pages/add_box/select_device/select_device_page.dart
@@ -18,11 +18,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:super_green_app/data/rel/rel_db.dart';
 import 'package:super_green_app/main/main_navigator_bloc.dart';
 import 'package:super_green_app/pages/add_box/select_device/select_device_bloc.dart';
 import 'package:super_green_app/widgets/appbar.dart';
+import 'package:super_green_app/widgets/fullscreen.dart';
+import 'package:super_green_app/widgets/green_button.dart';
 import 'package:super_green_app/widgets/section_title.dart';
 
 class SelectBoxDeviceData {
@@ -44,55 +45,64 @@ class _SelectDevicePageState extends State<SelectDevicePage> {
         bloc: BlocProvider.of<SelectDeviceBloc>(context),
         builder: (context, state) => Scaffold(
             appBar: SGLAppBar(
-              'Select Box device',
+              'SELECT BOX\'S DEVICE',
+              backgroundColor: Colors.orange,
+              titleColor: Colors.white,
+              iconColor: Colors.white,
             ),
-            body: Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: Column(
-                children: [
-                  SectionTitle(
-                      title: 'Select the device below',
-                      icon: 'assets/box_setup/icon_controller.svg'),
-                  Expanded(child: _deviceList(context)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      FlatButton(
-                        textColor: Colors.red,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(Icons.close),
-                            Text('NO SGL DEVICE'),
-                          ],
-                        ),
-                        onPressed: () {
-                          BlocProvider.of<MainNavigatorBloc>(context)
-                              .add(MainNavigatorActionPop(param: false));
-                        },
+            body: Column(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 100),
+                  height: 100,
+                  color: Colors.orange,
+                ),
+                SectionTitle(
+                  title: 'Select the device below',
+                  icon: 'assets/box_setup/icon_controller.svg',
+                  backgroundColor: Colors.orange,
+                  titleColor: Colors.white,
+                  large: true,
+                ),
+                Expanded(child: _deviceList(context)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FlatButton(
+                      textColor: Colors.red,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.close),
+                          Text('NO SGL DEVICE'),
+                        ],
                       ),
-                      FlatButton(
-                        textColor: Colors.blue,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(Icons.add),
-                            Text('ADD NEW DEVICE'),
-                          ],
-                        ),
-                        onPressed: () {
-                          BlocProvider.of<MainNavigatorBloc>(context).add(
-                              MainNavigateToAddDeviceEvent(
-                                  futureFn: (future) async {
-                            Device device = await future;
-                            if (device != null) {
-                              _selectDevice(context, device);
-                            }
-                          }));
-                        },
+                      onPressed: () {
+                        BlocProvider.of<MainNavigatorBloc>(context)
+                            .add(MainNavigatorActionPop(param: false));
+                      },
+                    ),
+                    FlatButton(
+                      textColor: Colors.blue,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.add),
+                          Text('ADD NEW DEVICE'),
+                        ],
                       ),
-                    ],
-                  )
-                ],
-              ),
+                      onPressed: () {
+                        BlocProvider.of<MainNavigatorBloc>(context).add(
+                            MainNavigateToAddDeviceEvent(
+                                futureFn: (future) async {
+                          Device device = await future;
+                          if (device != null) {
+                            _selectDevice(context, device);
+                          }
+                        }));
+                      },
+                    ),
+                  ],
+                )
+              ],
             )));
   }
 
@@ -107,6 +117,13 @@ class _SelectDevicePageState extends State<SelectDevicePage> {
           devices = state.devices;
         }
         int i = 1;
+        if (devices.length == 0) {
+          return Fullscreen(
+            title: 'No device yet',
+            child: GreenButton(title: 'ADD ONE', onPressed: () {}),
+            childFirst: false,
+          );
+        }
         return ListView(
           children: devices
               .map((
