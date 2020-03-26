ead27aba60465f524a28c01466d47d507c821964
vitaminwater <stantin04@gmail.com>
2020-03-26 18:41:39 +0100

    WIP - renaming all box concept to plant

---
+++ lib/towelie/buttons/towelie_button_plant_not_started.dart
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'package:super_green_app/towelie/towelie_button.dart';
+import 'package:super_green_app/towelie/towelie_bloc.dart';
+import 'package:super_green_app/towelie/towelie_cards_factory.dart';
+
+class TowelieButtonPlantNotStarted extends TowelieButton {
+  static Map<String, dynamic> createButton() {
+    return {
+      'ID': 'PLANT_NOT_STARTED',
+      'title': 'No',
+    };
+  }
+
+  @override
+  Stream<TowelieBlocState> buttonPressed(
+      TowelieBlocEventCardButtonPressed event) async* {
+    if (event.params['ID'] == 'PLANT_NOT_STARTED') {
+      await TowelieCardsFactory.createPlantTutoTakePic(event.feed);
+      await removeButtons(event.feedEntry);
+    }
+  }
+}
