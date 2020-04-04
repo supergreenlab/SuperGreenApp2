59a9887f6351916bce5041b6904bfd7de00f578d
vitaminwater <stantin04@gmail.com>
2020-04-04 08:50:18 +0200

    version bump, filter analytics events sent

--- lib/main/analytics_bloc_delegate.dart
+++ lib/main/analytics_bloc_delegate.dart
@@ -1,10 +1,31 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_matomo/flutter_matomo.dart';
+import 'package:super_green_app/device_daemon/device_daemon_bloc.dart';
+import 'package:super_green_app/pages/feed_entries/feed_media/card/feed_media_card_bloc.dart';
+import 'package:super_green_app/pages/feeds/feed/feed_bloc.dart';
+import 'package:super_green_app/pages/feeds/plant_feed/app_bar/plant_feed_app_bar_bloc.dart';
+import 'package:super_green_app/pages/feeds/plant_feed/plant_drawer_bloc.dart';
+import 'package:super_green_app/pages/feeds/plant_feed/plant_feed_bloc.dart';
+
+List<bool Function(Equatable)> filteredEvents = [
+  (e) => e is DeviceDaemonBlocEventDeviceReachable,
+  (e) => e is PlantFeedAppBarBlocEventReloadChart,
+  (e) => e is PlantFeedAppBarBlocEventLoadChart,
+  (e) => e is FeedBlocEventFeedEntriesListUpdated,
+  (e) => e is DeviceDaemonBlocEventLoadDevice,
+  (e) => e is PlantDrawerBlocEventBoxListUpdated,
+  (e) => e is FeedMediaCardBlocEventInit,
+  (e) => e is PlantFeedBlocEventLoad,
+];
 
 class AnalyticsBlocDelegate extends BlocDelegate {
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
+    if (filteredEvents.firstWhere((fn) => fn(transition.event), orElse: null) != null) {
+      return;
+    }
     FlutterMatomo.trackEventWithName('AnalyticsBlocDelegate', 'onTransition',
         transition.event.runtimeType.toString());
   }
