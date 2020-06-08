21acfa6617323e1934f6386dec3d178609d9ff44
vitaminwater <stantin04@gmail.com>
2020-06-08 14:11:35 +0200

    removed useless code

--- lib/data/api/device_api.dart
+++ lib/data/api/device_api.dart
@@ -27,8 +27,7 @@ import 'package:super_green_app/data/rel/device/devices.dart';
 import 'package:super_green_app/data/rel/rel_db.dart';
 
 class DeviceAPI {
-  static Client client;
-
+  
   static String mdnsDomain(String name) {
     return name
         .toLowerCase()
