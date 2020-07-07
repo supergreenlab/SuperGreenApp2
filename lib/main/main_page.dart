b422fd8aed7832a9039fea800366fc2902ff4f6c
vitaminwater <stantin04@gmail.com>
2020-07-07 09:53:36 +0200

    cleaning the folder tree

--- lib/main/main_page.dart
+++ lib/main/main_page.dart
@@ -74,8 +74,8 @@ import 'package:super_green_app/pages/feed_entries/feed_ventilation/form/feed_ve
 import 'package:super_green_app/pages/feed_entries/feed_ventilation/form/feed_ventilation_form_page.dart';
 import 'package:super_green_app/pages/feed_entries/feed_water/form/feed_water_form_bloc.dart';
 import 'package:super_green_app/pages/feed_entries/feed_water/form/feed_water_form_page.dart';
-import 'package:super_green_app/pages/feeds/public_plant/public_plant_bloc.dart';
-import 'package:super_green_app/pages/feeds/public_plant/public_plant_page.dart';
+import 'package:super_green_app/pages/feeds/plant_feeds/remote/public_plant_bloc.dart';
+import 'package:super_green_app/pages/feeds/plant_feeds/remote/public_plant_page.dart';
 import 'package:super_green_app/pages/fullscreen_media/fullscreen_media_bloc.dart';
 import 'package:super_green_app/pages/fullscreen_media/fullscreen_media_page.dart';
 import 'package:super_green_app/pages/fullscreen_picture/fullscreen_picture_page.dart';
