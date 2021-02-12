5623250e14090fc218c024917207df9c16f49cca
vitaminwater <stantin04@gmail.com>
2021-02-12 12:43:28 +0100

    wip - l10n

--- lib/pages/feed_entries/feed_life_event/form/feed_life_event_form_page.dart
+++ lib/pages/feed_entries/feed_life_event/form/feed_life_event_form_page.dart
@@ -21,6 +21,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
 import 'package:super_green_app/data/kv/app_db.dart';
+import 'package:super_green_app/l10n.dart';
 import 'package:super_green_app/main/main_navigator_bloc.dart';
 import 'package:super_green_app/pages/feed_entries/feed_life_event/form/feed_life_event_form_bloc.dart';
 import 'package:super_green_app/widgets/feed_form/feed_form_layout.dart';
@@ -28,15 +29,97 @@ import 'package:super_green_app/widgets/feed_form/feed_form_param_layout.dart';
 import 'package:super_green_app/widgets/fullscreen_loading.dart';
 import 'package:tuple/tuple.dart';
 
-const List<Tuple2<String, String>> phasesTitles = [
-  Tuple2('Germination date', 'assets/plant_infos/icon_germination_date.svg'),
-  Tuple2('Vegging since', 'assets/plant_infos/icon_vegging_since.svg'),
-  Tuple2('Blooming since', 'assets/plant_infos/icon_blooming_since.svg'),
-  Tuple2('Drying since', 'assets/plant_infos/icon_drying_since.svg'),
-  Tuple2('Curing since', 'assets/plant_infos/icon_curing_since.svg'),
+List<Tuple2<String, String>> phasesTitles = [
+  Tuple2(
+      FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabelGermination, 'assets/plant_infos/icon_germination_date.svg'),
+  Tuple2(FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabelVegging, 'assets/plant_infos/icon_vegging_since.svg'),
+  Tuple2(FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabelBlooming, 'assets/plant_infos/icon_blooming_since.svg'),
+  Tuple2(FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabelDrying, 'assets/plant_infos/icon_drying_since.svg'),
+  Tuple2(FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabelCuring, 'assets/plant_infos/icon_curing_since.svg'),
 ];
 
 class FeedLifeEventFormPage extends StatefulWidget {
+  static String get feedLifeEventFormPagePhaseLabelGermination {
+    return Intl.message(
+      'Germination date',
+      name: 'feedLifeEventFormPagePhaseLabelGermination',
+      desc: 'Life event germination label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPagePhaseLabelVegging {
+    return Intl.message(
+      'Vegging since',
+      name: 'feedLifeEventFormPagePhaseLabelVegging',
+      desc: 'Life event vegging label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPagePhaseLabelBlooming {
+    return Intl.message(
+      'Blooming since',
+      name: 'feedLifeEventFormPagePhaseLabelBlooming',
+      desc: 'Life event blooming label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPagePhaseLabelDrying {
+    return Intl.message(
+      'Drying since',
+      name: 'feedLifeEventFormPagePhaseLabelDrying',
+      desc: 'Life event drying label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPagePhaseLabelCuring {
+    return Intl.message(
+      'Curing since',
+      name: 'feedLifeEventFormPagePhaseLabelCuring',
+      desc: 'Life event curing label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPagePhaseLabel {
+    return Intl.message(
+      'Phase',
+      name: 'feedLifeEventFormPagePhaseLabel',
+      desc: 'Label for phase',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPageChangeButton {
+    return Intl.message(
+      'change',
+      name: 'feedLifeEventFormPageChangeButton',
+      desc: '"Change" button label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPageNotSet {
+    return Intl.message(
+      'Not set',
+      name: 'feedLifeEventFormPageNotSet',
+      desc: 'Displayed when date is not set yet',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
+  static String get feedLifeEventFormPageSetButton {
+    return Intl.message(
+      'set',
+      name: 'feedLifeEventFormPageSetButton',
+      desc: '"Set" button label',
+      locale: SGLLocalizations.current.localeName,
+    );
+  }
+
   @override
   _FeedLifeEventFormPageState createState() => _FeedLifeEventFormPageState();
 }
@@ -60,7 +143,8 @@ class _FeedLifeEventFormPageState extends State<FeedLifeEventFormPage> {
           cubit: BlocProvider.of<FeedLifeEventFormBloc>(context),
           builder: (context, state) {
             Widget body;
-            Tuple2<String, String> phaseTitle = Tuple2('Phase', 'assets/plant_infos/icon_germination_date.svg');
+            Tuple2<String, String> phaseTitle = Tuple2(
+                FeedLifeEventFormPage.feedLifeEventFormPagePhaseLabel, 'assets/plant_infos/icon_germination_date.svg');
             if (state is FeedLifeEventFormBlocStateInit || state is FeedLifeEventFormBlocStateDone) {
               body = Expanded(child: FullscreenLoading());
             } else if (state is FeedLifeEventFormBlocStateLoaded) {
@@ -91,10 +175,10 @@ class _FeedLifeEventFormPageState extends State<FeedLifeEventFormPage> {
       String format = AppDB().getAppData().freedomUnits ? 'MM/dd/yyyy' : 'dd/MM/yyyy';
       DateFormat f = DateFormat(format);
       text = f.format(date);
-      buttonText = 'change';
+      buttonText = FeedLifeEventFormPage.feedLifeEventFormPageChangeButton;
     } else {
-      text = 'Not set';
-      buttonText = 'set';
+      text = FeedLifeEventFormPage.feedLifeEventFormPageNotSet;
+      buttonText = FeedLifeEventFormPage.feedLifeEventFormPageSetButton;
     }
     return Padding(
       padding: const EdgeInsets.all(16.0),
