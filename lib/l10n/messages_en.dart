82945d85223b8e21b7b938d5dbc81952468a85e7
vitaminwater <stantin04@gmail.com>
2021-03-03 11:50:50 +0100

    wording

--- lib/l10n/messages_en.dart
+++ lib/l10n/messages_en.dart
@@ -75,7 +75,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static m27(name) => "Controller ${name} refreshed!";
 
-  static m28(count) => "${Intl.plural(count, one: '1 like', other: '${count} likes')}";
+  static m28(count) => "${Intl.plural(count, one: 'Liked by 1', other: 'Liked by ${count}')}";
 
   static m29(time) => "(100% power for ${time} s)";
 
