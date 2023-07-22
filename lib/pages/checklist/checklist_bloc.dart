280c92d5ab81de2a650c4428c029a6aced2b1278
vitaminwater <stantin04@gmail.com>
2023-07-22 07:37:03 +0200

    wip

--- lib/pages/checklist/checklist_bloc.dart
+++ lib/pages/checklist/checklist_bloc.dart
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:super_green_app/data/rel/rel_db.dart';
 import 'package:super_green_app/main/main_navigator_bloc.dart';
@@ -26,6 +28,15 @@ abstract class ChecklistBlocEvent extends Equatable {}
 class ChecklistBlocEventInit extends ChecklistBlocEvent {
   @override
   List<Object> get props => [];
+}
+
+class ChecklistBlocEventChecklistSeedsChanged extends ChecklistBlocEvent {
+  final List<ChecklistSeed> checklistSeeds;
+
+  ChecklistBlocEventChecklistSeedsChanged(this.checklistSeeds);
+
+  @override
+  List<Object> get props => [checklistSeeds,];
 }
 
 abstract class ChecklistBlocState extends Equatable {}
@@ -48,6 +59,8 @@ class ChecklistBlocStateLoaded extends ChecklistBlocState {
 class ChecklistBloc extends LegacyBloc<ChecklistBlocEvent, ChecklistBlocState> {
   final MainNavigateToChecklist args;
 
+  late StreamSubscription sub;
+
   ChecklistBloc(this.args) : super(ChecklistBlocStateInit()) {
     add(ChecklistBlocEventInit());
   }
@@ -56,7 +69,20 @@ class ChecklistBloc extends LegacyBloc<ChecklistBlocEvent, ChecklistBlocState> {
   Stream<ChecklistBlocState> mapEventToState(ChecklistBlocEvent event) async* {
     if (event is ChecklistBlocEventInit) {
       List<ChecklistSeed> checklistSeeds = await RelDB.get().checklistsDAO.getChecklistSeeds(this.args.checklist.id);
+      sub = RelDB.get().checklistsDAO.watchChecklistSeeds(this.args.checklist.id).listen((onChecklistSeedsChanged));
       yield ChecklistBlocStateLoaded(this.args.checklist, checklistSeeds);
+    } else if (event is ChecklistBlocEventChecklistSeedsChanged) {
+      yield ChecklistBlocStateLoaded(this.args.checklist, event.checklistSeeds);
     }
+  }
+
+  void onChecklistSeedsChanged(List<ChecklistSeed> seeds) {
+    add(ChecklistBlocEventChecklistSeedsChanged(seeds));
+  }
+
+  @override
+  Future<void> close() async {
+    await sub.cancel();
+    return super.close();
   }
 }
