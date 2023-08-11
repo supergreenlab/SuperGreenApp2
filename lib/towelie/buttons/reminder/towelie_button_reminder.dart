9f3e43a764ededb21f46f304d2776bc1ea0e8e21
vitaminwater <stantin04@gmail.com>
2023-08-11 09:24:07 +0200

    towelie now also creates a checklist seed for watering reminders

--- lib/towelie/buttons/reminder/towelie_button_reminder.dart
+++ lib/towelie/buttons/reminder/towelie_button_reminder.dart
@@ -16,6 +16,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'dart:convert';
+
+import 'package:drift/drift.dart';
+import 'package:super_green_app/data/assets/feed_entry.dart';
+import 'package:super_green_app/data/kv/app_db.dart';
+import 'package:super_green_app/data/rel/checklist/actions.dart';
+import 'package:super_green_app/data/rel/checklist/categories.dart';
+import 'package:super_green_app/data/rel/checklist/conditions.dart';
+import 'package:super_green_app/data/rel/rel_db.dart';
 import 'package:super_green_app/notifications/model.dart';
 import 'package:super_green_app/notifications/notifications.dart';
 import 'package:super_green_app/towelie/towelie_bloc.dart';
@@ -41,9 +50,48 @@ class TowelieButtonReminder extends TowelieButton {
   Stream<TowelieBlocState> buttonPressed(TowelieBlocEventButtonPressed event) async* {
     NotificationsBloc.localNotifications.reminderNotification(event.params['notificationID'],
         event.params['afterMinutes'], NotificationData.fromJSON(event.params['notificationPayload']));
+    NotificationDataReminder notificationData =
+        NotificationDataReminder.fromMap(json.decode(event.params['notificationPayload']));
+
     if (event.feedEntry != null) {
       await selectButtons(event.feedEntry,
           selector: (params) => params['afterMinutes'] == event.params['afterMinutes']);
     }
+
+    if (AppDB().getAppData().jwt == null) {
+      return;
+    }
+
+    Checklist? checklist;
+    try {
+      checklist = await RelDB.get().checklistsDAO.getChecklistForPlant(notificationData.plantID);
+    } catch (e) {}
+    if (checklist == null) {
+      int checklistID = await RelDB.get().checklistsDAO.addChecklist(ChecklistsCompanion.insert(
+            plant: notificationData.plantID,
+            synced: Value(false),
+          ));
+      checklist = await RelDB.get().checklistsDAO.getChecklist(checklistID);
+    }
+
+    ChecklistActionCreateCard action = ChecklistActionCreateCard(
+      entryType: FE_WATER,
+    );
+    ChecklistCondition condition = ChecklistConditionTimer(
+      date: DateTime.now().add(Duration(days: 1)),
+    );
+    ChecklistSeedsCompanion reminder = ChecklistSeedsCompanion.insert(
+      checklist: checklist.id,
+      checklistServerID: Value(checklist.serverID),
+      title: Value('Water plant reminder'),
+      category: Value(CH_FEEDING),
+      public: Value(false),
+      repeat: Value(false),
+      conditions: Value('[${condition.toJSON()}]'),
+      exitConditions: Value('[]'),
+      actions: Value('[${action.toJSON()}]'),
+      synced: Value(false),
+    );
+    await RelDB.get().checklistsDAO.addChecklistSeed(reminder);
   }
 }
