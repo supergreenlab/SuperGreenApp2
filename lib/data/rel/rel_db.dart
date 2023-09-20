a4aafb2c4ca22190372eb345499be7cbaf200ca7
vitaminwater <stantin04@gmail.com>
2023-09-20 16:26:34 +0200

    added fast default value

--- lib/data/rel/rel_db.dart
+++ lib/data/rel/rel_db.dart
@@ -141,6 +141,8 @@ class RelDB extends _$RelDB {
       await m.createTable(checklistLogs);
     } else if (fromVersion == 16) {
       await m.addColumn(checklists, checklistSeeds.fast);
+            await m.issueCustomQuery(
+          "update checklist_seeds set fast = false");
       await m.addColumn(checklistSeeds, checklistSeeds.collection);
       await m.addColumn(checklistSeeds, checklistSeeds.checklistCollectionServerID);
       await m.addColumn(checklistLogs, checklistLogs.noRepeat);
